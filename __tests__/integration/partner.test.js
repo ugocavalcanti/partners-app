b8fe93b88c6d4bc856c3da92c3c87a318fc16b54
Ugo Cavalcanti <ugocavalcanti@gmail.com>
2021-04-24 10:28:44 -0300

    tests corrections

--- __tests__/integration/partner.test.js
+++ __tests__/integration/partner.test.js
@@ -3,17 +3,24 @@ const app = require("../../api");
 const { factory, partnerObject } = require("../utils/factories");
 const Partner = require("../../models/Partner");
 
+
+afterAll(async () => {
+  await Partner.deleteMany();
+  factory.cleanUp();
+  Partner.db.close();
+})
+
 describe("Create Partner",  () => {
-  afterAll(async () => {
-    await Partner.remove({})
-  })
-  it("should create a partner in database", async () => {
-      const response = await request(app)
-          .post("/partner")
-          .send(partnerObject);
-  
-          expect(response.statusCode).toBe(200);
-          expect(response.body.data.document).toBe("05202839000150");      
+  it("should create a partner in database", async (done) => {
+     
+       const response = await request(app)
+             .post("/partner")
+             .send(partnerObject);
+   
+        expect(response.statusCode).toBe(200);
+        expect(response.body.data.document).toBe("05202839000150");   
+        
+        done();
   });
 
   it("shouldn't create a partner in database with the same document", async () => {
@@ -25,8 +32,8 @@ describe("Create Partner",  () => {
           expect(response.body).toHaveProperty("error"); 
   });
 
-  it("shouldn't create a partner in database with polygon opened", async () => {
-    const response = await request(app)
+    it("shouldn't create a partner in database with polygon opened", async () => {
+      const response = await request(app)
         .post("/partner")
         .send({
             ...partnerObject,
@@ -40,9 +47,8 @@ describe("Create Partner",  () => {
 
         expect(response.statusCode).toBe(500);
         expect(response.body).toHaveProperty("error");      
-  });
+    });
   
-
     it("shouldn't create a partner in database that don't respect the enum", async () => {
         const response = await request(app)
             .post("/partner")
@@ -58,9 +64,9 @@ describe("Create Partner",  () => {
     
             expect(response.statusCode).toBe(500);
             expect(response.body).toHaveProperty("error");      
-    });
+      });
 
-    it("shouldn't create a partner in database without required fields - coverageArea", async () => {
+      it("shouldn't create a partner in database without required fields - coverageArea", async () => {
         const response = await request(app)
             .post("/partner")
             .send({
@@ -86,23 +92,18 @@ describe("Create Partner",  () => {
 });
 
 describe("Get Partner by id", () => {
-    afterAll(() => {
-      factory.cleanUp();
-    })
 
     it("should get a partner by id", async () => {
+      
+      const partner = await factory.create("Partner", {
+          document: "05202839000151"
+      });
 
-        const partner = await factory.create("Partner", {
-            document: "05202839000151"
-        });
+      const id = partner._id;
 
-        const id = partner._id;
-
-        const responseGet = await request(app)
-        .get(`/partner/${id}`);
-
-        expect(responseGet.statusCode).toBe(200);
-        expect(responseGet.body.data._id.toString()).toBe(id.toString());
+      const responseGet = await request(app).get(`/partner/${id}`);
+      expect(responseGet.statusCode).toBe(200);
+      expect(responseGet.body.data._id.toString()).toBe(id.toString());
     });
 
     it("should get a null data when the id doesn't exist in database", async () => {
@@ -117,95 +118,91 @@ describe("Get Partner by id", () => {
 });
 
 describe("Search Partner by coordinates", () => {
-    afterAll(() => {
-      factory.cleanUp();
-    })
 
     it("should get a nearest partner of a specific point", async () => {
 
-        await factory.create("Partner", {
-            document: "05202839000101"
-        });
-        await factory.create("Partner", {
-            document: "05202839000102",
-            coverageArea: {
-                "type": "MultiPolygon",
-                "coordinates": [
+      await factory.create("Partner", {
+          document: "05202839000101"
+      });
+      await factory.create("Partner", {
+          document: "05202839000102",
+          coverageArea: {
+              "type": "MultiPolygon",
+              "coordinates": [
+                [
                   [
                     [
-                      [
-                        -43.400630950927734,
-                        -22.759403224505906
-                      ],
-                      [
-                        -43.31806182861328,
-                        -22.759403224505906
-                      ],
-                      [
-                        -43.31806182861328,
-                        -22.718081880968413
-                      ],
-                      [
-                        -43.400630950927734,
-                        -22.718081880968413
-                      ],
-                      [
-                        -43.400630950927734,
-                        -22.759403224505906
-                      ]
+                      -43.400630950927734,
+                      -22.759403224505906
+                    ],
+                    [
+                      -43.31806182861328,
+                      -22.759403224505906
+                    ],
+                    [
+                      -43.31806182861328,
+                      -22.718081880968413
+                    ],
+                    [
+                      -43.400630950927734,
+                      -22.718081880968413
+                    ],
+                    [
+                      -43.400630950927734,
+                      -22.759403224505906
                     ]
                   ]
                 ]
-              },
-              address: {
-                "type": "Point",
-                "coordinates": [-43.3445,-22.7401]
-              }
-        });
-        await factory.create("Partner", {
-            document: "05202839000103",
-            coverageArea: {
-                "type": "MultiPolygon",
-                "coordinates": [
+              ]
+            },
+            address: {
+              "type": "Point",
+              "coordinates": [-43.3445,-22.7401]
+            }
+      });
+      await factory.create("Partner", {
+          document: "05202839000103",
+          coverageArea: {
+              "type": "MultiPolygon",
+              "coordinates": [
+                [
                   [
                     [
-                      [
-                        -43.44921112060547,
-                        -22.735340206036337
-                      ],
-                      [
-                        -43.370933532714844,
-                        -22.735340206036337
-                      ],
-                      [
-                        -43.370933532714844,
-                        -22.689735505952164
-                      ],
-                      [
-                        -43.44921112060547,
-                        -22.689735505952164
-                      ],
-                      [
-                        -43.44921112060547,
-                        -22.735340206036337
-                      ]
+                      -43.44921112060547,
+                      -22.735340206036337
+                    ],
+                    [
+                      -43.370933532714844,
+                      -22.735340206036337
+                    ],
+                    [
+                      -43.370933532714844,
+                      -22.689735505952164
+                    ],
+                    [
+                      -43.44921112060547,
+                      -22.689735505952164
+                    ],
+                    [
+                      -43.44921112060547,
+                      -22.735340206036337
                     ]
                   ]
                 ]
-              },
-              address: {
-                "type": "Point",
-                "coordinates": [-43.4145,-22.7106 ]
-              }
-        });
-        
-        const long = -43.3829;
-        const lat = -22.7182;
-        const {body, statusCode} = await request(app)
-            .get(`/partner/${long}/${lat}`);
-    
-            expect(statusCode).toBe(200);
-            expect(body.data).not.toBeNull();
+              ]
+            },
+            address: {
+              "type": "Point",
+              "coordinates": [-43.4145,-22.7106 ]
+            }
+      });
+      
+      const long = -43.3829;
+      const lat = -22.7182;
+      const {body, statusCode} = await request(app).get(`/partner/${long}/${lat}`);
+  
+          expect(statusCode).toBe(200);
+          expect(body.data).not.toBeNull();
     });
 
     it("shouldn't get a partner if the point isn't coveraged ", async () => {
