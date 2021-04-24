45df946797a4d6ed5583eaf53614178cc72f50f5
Ugo Cavalcanti <ugocavalcanti@gmail.com>
2021-04-24 10:25:21 -0300

    field beil

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   // automock: false,
 
   // Stop running tests after `n` failures
-  bail: 1,
+  bail: true,
 
   // The directory where Jest should store its cached dependency information
   // cacheDirectory: "C:\\Ugo\\dev\\vs code\\data\\tmp\\jest",
